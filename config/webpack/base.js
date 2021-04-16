6cffb74b9777d1330536c117e3d6fb555769b71f
dotRamsey <dotramsey87@gmail.com>
2021-04-16 23:12:48 +0200

    Update base.js

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,4 +1,8 @@
 const { webpackConfig, merge } = require('@rails/webpacker')
-const customConfig = require('./custom')
+const customConfig = {
+  resolve: {
+    extensions: ['.css']
+  }
+}
 
 module.exports = merge(webpackConfig, customConfig)
